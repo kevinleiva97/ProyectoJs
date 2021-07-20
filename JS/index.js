7a8a67682afd1a72f6c5ffbc354275737a3671bd
Kevin <KevinLeiva97@gmail.com>
2021-07-20 01:38:44 -0600

    add required fields v1

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,37 @@
 'use strict'
 
-const btnRegistrar = document.querySelector('#btn-task')
+const btnRegistrar = document.querySelector('#btn-task');
+const inputDate = document.querySelector('#date-Input');
+const inputName = document.querySelector('#name-Input');
+const priority = document.querySelector('#priority');
+const attendantBy = document.querySelector('#attendant');
+const description = document.querySelector('#description');
+
+
 
 const validar = () => {
-    console.log(`prueba2`)
-    Swal.fire('Error!', 'All fields are required', 'error');
+
+    const camposRequeridos = document.querySelectorAll('#frm-Resgitrar input:required');
+    let error = false;
+
+    camposRequeridos.forEach(campo => {
+        if (campo.value == '') {
+            error = true;
+        }
+    });
+
+    if (error == true) {
+        Swal.fire({
+            'icon': 'error',
+            'title': 'All Fields are Required',
+            'text': `Can't insert a new task with empty Fields`,
+            'confirmButtonText': 'Ok'
+        });
+
+    } else {
+        resgitrarTarea();
+
+    }
 }
 
-btnRegistrar.addEventListener('click', validar)
+btnRegistrar.addEventListener('click', validar);
---
+++ JS/task.js
@@ -0,0 +1,10 @@
+let tasks = [{
+        "date": "19 / 07 / 2021",
+        "name": "Start task",
+        "priority": "High",
+        "attendant": "Kevin",
+        "description": "Esta tarea es de prueba para crear el JSON"
+
+    }
+
+];
