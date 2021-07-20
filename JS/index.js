dbdf7524bd5873f5d36141ec03348be112ad7013
Kevin <KevinLeiva97@gmail.com>
2021-07-20 01:11:54 -0600

    add js file v1

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const btnRegistrar = document.querySelector('#btn-task')
+
+const validar = () => {
+    console.log(`prueba2`)
+    Swal.fire('Error!', 'All fields are required', 'error');
+}
+
+btnRegistrar.addEventListener('click', validar)
